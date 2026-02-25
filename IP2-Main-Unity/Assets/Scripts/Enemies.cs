73177da62cf1426417d32097657f016920bc6426
CameronMacBeath <cambot6000@gmail.com>
2026-02-25 20:30:44 +0000

    Updated Hopper and Game ui

--- IP2-Main-Unity/Assets/Scripts/Enemies.cs
+++ IP2-Main-Unity/Assets/Scripts/Enemies.cs
@@ -1,17 +1,21 @@
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Enemies : MonoBehaviour
 {
-    public int health = 0;
+    public float health = 0;
     public float speed = 100;
+    public int damage = 5;
     public Vector3 target;
     public List<Vector3> pathWaypoint = new List<Vector3>();
     public int waypointNumber;
 
+    private GameUI gameUI;
     private void Start()
     {
         EnemiesSpawner grid = FindAnyObjectByType<EnemiesSpawner>();
+        gameUI =FindAnyObjectByType<GameUI>();
         if (grid == null)
         {
             Debug.LogError("GridLayout not assigned!");
@@ -28,16 +32,9 @@ public class Enemies : MonoBehaviour
         }
 
         target = pathWaypoint[waypointNumber];
-        for (int i = 0; i < pathWaypoint.Count; i++)
-        {
-            pathWaypoint[i] = new Vector3(
-                 pathWaypoint[i].x,
-                 transform.position.y,
-                 pathWaypoint[i].z
-                );
+        
 
-
-        }
+        
     }
 
 
@@ -46,18 +43,24 @@ public class Enemies : MonoBehaviour
 
         float step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, target, step);
+        Vector3 direction = (target - transform.position).normalized;
+        quaternion targetRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.RotateTowards(
+        transform.rotation,
+        targetRotation,
+        100 * Time.deltaTime);
+
+
         if ((Vector3)transform.position == target)
         {
             waypointNumber++;
             ChangeWaypoint();
         }
-        if (health <= 0)
-        {
-            Destroy(gameObject);
-        }
+   
 
         if (waypointNumber == pathWaypoint.Count - 1)
         {
+            gameUI.TakeDamage(damage);
             Destroy(gameObject);
         }
     }
@@ -73,5 +76,6 @@ public class Enemies : MonoBehaviour
     }
 
 }
+
 
 
