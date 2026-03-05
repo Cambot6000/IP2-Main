ba331ffd1e1bd23bf17f92d20865d6e58891fb92
Skorp-999 <owenmichael.dixie@gmail.com>
2026-03-05 16:38:59 +0000

    poison explanation

--- IP2-Main-Unity/Assets/Scripts/PoisonousScript.cs
+++ IP2-Main-Unity/Assets/Scripts/PoisonousScript.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 
 public class PoisonousScript : MonoBehaviour
 {
-    public int poisonTimes;
-    public int poisonDamage;
-    public int poisonWait;
+    public int poisonTimes; //How many times the enemy will take poison damage before the effect wears off
+    public int poisonDamage; //How much damage the poison deals per poison "tick"
+    public int poisonWait; //How long it takes for the enemy to take damage from poison
     [SerializeField] private bool pool;
-    public float lifeSpan;
+    public float lifeSpan; //How long a poison pool lasts for before disappearing
 
     private void Start()
     {
