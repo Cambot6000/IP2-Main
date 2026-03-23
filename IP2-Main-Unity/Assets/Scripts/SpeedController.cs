c74e5b87bd7eada2e143bfed8539972b435059b6
Skorp-999 <owenmichael.dixie@gmail.com>
2026-03-23 17:41:39 +0000

    fade and nest model 1 implemented

--- IP2-Main-Unity/Assets/Scripts/SpeedController.cs
+++ IP2-Main-Unity/Assets/Scripts/SpeedController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -8,11 +9,14 @@ public class SpeedController : MonoBehaviour
     public int buttonIndex;
     private float currentSpeed;
     public float targetSpeed;
+    public float fadeDuration = 0.2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        speedButtons[0].enabled = true;
-        speedButtons[1].enabled = false;
+        SetImageTransparency(speedButtons[0], 1f);
+        SetImageTransparency(speedButtons[1], 0f);
+        //speedButtons[0].enabled = true;
+        //speedButtons[1].enabled = false;
     }
 
     // Update is called once per frame
@@ -29,17 +33,21 @@ public class SpeedController : MonoBehaviour
             //Debug.Log("speed me up");
             if (buttonIndex == 0 && currentSpeed < targetSpeed) 
             {
-                speedButtons[buttonIndex].enabled = false;
+                //speedButtons[buttonIndex].enabled = false;
+                StartCoroutine(FadeButton(speedButtons[buttonIndex], 0f));
                 buttonIndex = 1;
-                speedButtons[buttonIndex].enabled = true;
+                //speedButtons[buttonIndex].enabled = true;
+                StartCoroutine(FadeButton(speedButtons[buttonIndex], 1f));
                 currentSpeed = targetSpeed;
                 SpeedUp();
             }
             else if(buttonIndex == 1 && currentSpeed == targetSpeed)
             {
-                speedButtons[buttonIndex].enabled = false;
+                //speedButtons[buttonIndex].enabled = false;
+                StartCoroutine(FadeButton(speedButtons[buttonIndex], 0f));
                 buttonIndex = 0;
-                speedButtons[buttonIndex].enabled = true;
+                //speedButtons[buttonIndex].enabled = true;
+                StartCoroutine(FadeButton(speedButtons[buttonIndex], 1f));
                 currentSpeed = 1f;
                 SlowDown();
             }
@@ -54,5 +62,26 @@ public class SpeedController : MonoBehaviour
     void SlowDown()
     {
         Time.timeScale = 1f;
+    }
+
+    IEnumerator FadeButton(Image speedyDos, float targetTransparency)
+    {
+        float startTransparency = speedyDos.color.a;
+        float time = 0;
+
+        while (time < fadeDuration)
+        {
+            time += Time.unscaledDeltaTime; //Use unscaledDeltaTime because Time.timeScale changes becasue of button hits
+            float newAlpha = Mathf.Lerp(startTransparency, targetTransparency, time / fadeDuration);
+            SetImageTransparency(speedyDos, newAlpha);
+            yield return null;
+        }
+        SetImageTransparency(speedyDos, targetTransparency);
+    }
+    void SetImageTransparency(Image speedy, float transparency)
+    {
+        Color colour = speedy.color;
+        colour.a = transparency;
+        speedy.color = colour;
     }
 }
