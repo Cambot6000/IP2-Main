73954e38d2e71bac0b02be0a348df81c323af8e3
Skorp-999 <owenmichael.dixie@gmail.com>
2026-04-14 15:07:08 +0100

    it works.

--- IP2-Main-Unity/Assets/Scripts/PauseMenuScript.cs
+++ IP2-Main-Unity/Assets/Scripts/PauseMenuScript.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+using System.Collections;
 
 public class PauseMenuScript : MonoBehaviour
 {
     public GameObject ui;
+    public float speedBeforePause;
+    public GameObject resumeButton;
+    //public GameObject[] otherUIStuff; old code because I could't work out why controller wouldn't work on the pause menu
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) || Gamepad.current.startButton.wasPressedThisFrame)
+        if (Input.GetKeyDown(KeyCode.Escape) || Gamepad.current.startButton.wasPressedThisFrame) //Uses the settings button on ps controller
         {
             Toggle();
         }
@@ -20,11 +25,38 @@ public class PauseMenuScript : MonoBehaviour
 
         if (ui.activeSelf)
         {
+            /*
+            Cursor.lockState = CursorLockMode.Locked; probably not needed anymore
+            Cursor.visible = false;
+            */
+            speedBeforePause = Time.timeScale; //Used in case you're using the speed up thinghy
             Time.timeScale = 0f;
+            EventSystem.current.SetSelectedGameObject(null);
+            /*
+            foreach (GameObject sb2h in otherUIStuff)
+            {
+                if (sb2h != null)
+                {
+                    sb2h.SetActive(false);
+                }
+            }
+            */
+            //Above lines in commentary used to turn off all other UI, not needed anymore because I got it working properly
+            StartCoroutine(PleaseWork());
         }
         else
         {
-            Time.timeScale = 1f;
+            /*
+            foreach (GameObject sb2h in otherUIStuff)
+            {
+                if (sb2h != null)
+                {
+                    sb2h.SetActive(true);
+                }
+            }
+            */
+            //Above lines in commentary used to turn on all other UI, not needed anymore because I got it working properly
+            Time.timeScale = speedBeforePause;
         }
     }
    
@@ -36,13 +68,19 @@ public class PauseMenuScript : MonoBehaviour
     }
     public void Quit()
     {
-
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Main Menu");
     }
 
     public void Settings()
     {
-
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Settings Menu");
+    }
+    private IEnumerator PleaseWork() //Delays the selection of the resume button until the end of the frame so that it actually gets picked now
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(resumeButton);
     }
 }
